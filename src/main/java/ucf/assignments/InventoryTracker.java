750d7f7f2efefa59f68fd38928c513acf3bd6ae1
Paul Shannon <mikey4295@hotmail.com>
2021-07-20 20:40:41 -0400

    initial commit

---
+++ src/main/java/ucf/assignments/InventoryTracker.java
@@ -0,0 +1,24 @@
+package ucf.assignments;
+
+/*
+    Application is a GUI app
+
+    user able to store at least 10 inventory items
+    save and load their list of inventory items from external storage as CSV, HTML, or JSON - they can pick
+    sort inventory by value,serial number, and name
+    search for inventory item by a serial number or name
+    each item shall have a value representing its monitrary value in us dollars
+    have serialnumber hwere x can be a letter or digit xxxxxxxxx
+    serial number is unique
+    each item shall have a name consisting of at least 2 chars
+
+    user able to add a new inventory item
+    able to remove an existing item
+    able to edit value of item, serial number or name of item
+    able to print all inventory items in a tabular format as defined in following figure:
+
+        value   serial number   name
+        $399.99 AXB124AXY       Xbox One
+ */
+public class InventoryTracker {
+}
